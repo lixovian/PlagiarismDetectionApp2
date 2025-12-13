7f53e6c94362492b0a7bc145f8cf69dbf1a3469b
LIX <102993665+lixovian@users.noreply.github.com>
2025-12-14 02:40:15 +0300

    cors removed

--- FileStoringService.Host/Program.cs
+++ FileStoringService.Host/Program.cs
@@ -6,17 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton(TimeProvider.System);
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins("http://localhost:5002")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
-});
-
-
 builder.Services.AddOpenApi("api");
 
 builder.Services.AddInfrastructure();
