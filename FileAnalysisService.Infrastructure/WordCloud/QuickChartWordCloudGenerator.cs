6e901f232c143ff60aff2fb67f176b0edf5d330f
LIX <102993665+lixovian@users.noreply.github.com>
2025-12-14 03:21:45 +0300

    FIX

--- FileAnalysisService.Infrastructure/WordCloud/QuickChartWordCloudGenerator.cs
+++ FileAnalysisService.Infrastructure/WordCloud/QuickChartWordCloudGenerator.cs
@@ -7,20 +7,6 @@ internal sealed class QuickChartWordCloudGenerator : IWordCloudGenerator
 {
     public string Generate(string text)
     {
-        var payload = new
-        {
-            format = "png",
-            width = 500,
-            height = 500,
-            fontScale = 15,
-            removeStopwords = true,
-            text
-        };
-
-        var json = JsonSerializer.Serialize(payload);
-
-        var encoded = Uri.EscapeDataString(json);
-
-        return $"https://quickchart.io/wordcloud?c={encoded}";
+        return $"https://quickchart.io/wordcloud?text=\"{text}\"";
     }
 }
